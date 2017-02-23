f3a72cb8a4aeef63c6a71a7288daebbf249a5ebf
Mostafa Haque <93haque@gmail.com>
2017-02-23 17:04:54 -0500

    Runner level first draft

--- Runner_Game/Assets/Scripts/clickManager.cs
+++ Runner_Game/Assets/Scripts/clickManager.cs
@@ -11,6 +11,25 @@ public class clickManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 _rayPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D _rayHit = Physics2D.Raycast(new Vector2(_rayPosition.x, _rayPosition.y), Camera.main.transform.forward);
+            Debug.Log(_rayHit.collider.name);
+
+            switch (_rayHit.collider.tag)
+            {
+                case "fall":
+
+                    _rayHit.collider.GetComponent<Rigidbody2D>().gravityScale = 8;
+
+                    //_rayHit.collider.transform.position = Vector3.MoveTowards(_rayHit.collider.transform.position, new Vector3(_rayHit.collider.transform.position.x, _rayHit.collider.transform.position.y - 3, _rayHit.collider.transform.position.z), 0.5f);
+
+                    break;
+
+            }
+        }
+
+     
+    }
 }
--- Runner_Game/Assets/Scripts/runForestRun.cs
+++ Runner_Game/Assets/Scripts/runForestRun.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class runForestRun : MonoBehaviour {
  
     public float velocity=100;
+    public float velocityGain = 0.1f;
     private Transform tr;
     private Rigidbody2D rb;
 
@@ -21,6 +22,8 @@ public class runForestRun : MonoBehaviour {
 
 
         rb.velocity = new Vector2(velocity, rb.velocity.y);
+
+       velocity = velocity + velocity * velocityGain*Time.deltaTime;
 
         //tr.position += Vector3.right * Time.deltaTime * velocity;
 
